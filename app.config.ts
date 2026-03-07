9a51d24f6e528d441d3542cecabf9b27e8ec738f
Nazar Palamarchuk <104204125+nazar-pal@users.noreply.github.com>
2026-03-07 21:40:52 +0200

    Enhance app.config.ts for dynamic app configuration
    - Introduced environment-based app identifiers and names for development and preview variants.
    - Updated Expo configuration to include EAS project ID and app owner.
    - Added unique bundle identifier and support for tablet in iOS configuration.
    - Enhanced extra configuration for EAS integration.

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,29 @@
 import type { ConfigContext, ExpoConfig } from 'expo/config'
 
+const IS_DEV = process.env.EXPO_PUBLIC_APP_VARIANT === 'development'
+const IS_PREVIEW = process.env.EXPO_PUBLIC_APP_VARIANT === 'preview'
+
+const APP_ID_PREFIX = 'com.devnazar.svitlo'
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) return `${APP_ID_PREFIX}.dev`
+  if (IS_PREVIEW) return `${APP_ID_PREFIX}.preview`
+  return APP_ID_PREFIX // TODO: maybe `${APP_ID_PREFIX}.app` for production ?
+}
+
+const getAppName = () => {
+  if (IS_DEV) return 'Svitlo (Dev)'
+  if (IS_PREVIEW) return 'Svitlo (Preview)'
+  return 'Svitlo'
+}
+
+const EAS_PROJECT_ID = 'b2d90d72-b3bf-4519-a298-bca139cf9ce9'
+const EAS_APP_OWNER = 'devnazar'
+
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: 'svitlo',
+  owner: EAS_APP_OWNER,
+  name: getAppName(),
   slug: 'svitlo',
   version: '1.0.0',
   orientation: 'portrait',
@@ -11,7 +32,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   scheme: 'svitlo',
   userInterfaceStyle: 'automatic',
   ios: {
-    icon: './assets/expo.icon'
+    icon: './assets/expo.icon',
+    supportsTablet: false,
+    bundleIdentifier: getUniqueIdentifier()
   },
   web: {
     bundler: 'metro',
@@ -27,6 +50,11 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       }
     ]
   ],
+  extra: {
+    eas: {
+      projectId: EAS_PROJECT_ID
+    }
+  },
   experiments: {
     typedRoutes: true,
     reactCompiler: true
